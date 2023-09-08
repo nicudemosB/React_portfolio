49f61722d82dfda86c0f29710c1d9b16cd08df30
Billy Nicudemos <billynicudemos@Billys-MacBook-Air.local>
2023-09-07 22:44:31 -0400

    building portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,20 @@ export default function About() {
                     <p className='mb-8 leading-relaxed'>
                         lorem ipsem
                     </p>
+                    <div className='flex justify-center'>
+                        <a href='#contact' className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'>
+                            Lets Work!
+                        </a>
+                        <a href='#projects' className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'>
+                            See My Past Work
+                        </a>
+                    </div>
                 </div>
+                <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'></div>
+                <img
+                className='object-cover object-center rounded'
+                alt='hero'
+                src='./coding.svg'/>
             </div>
         </section>
     )
