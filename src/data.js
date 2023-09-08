b765bdf6e5d2c3c8d01ddf1d78c55ac807a56f5d
Billy Nicudemos <billynicudemos@Billys-MacBook-Air.local>
2023-09-08 09:07:11 -0400

    adding links to my other projects

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+{
+  title: 'Track Day',
+  subtitle: 'MERN',
+  description: 
+  'This is an app where a driver can sign up to join a race on the track',
+  image: '',
+  link: 'https://whispering-citadel-91521.herokuapp.com/',  
+},
+]
